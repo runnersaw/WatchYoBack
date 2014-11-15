0d612cdeec4ebd24d8c6228b1371f44b30d12ef8
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2014-11-15 13:57:29 -0500

    added basic links

--- web.py
+++ web.py
@@ -20,10 +20,20 @@ def begin():
 	searchword = request.args.get('username')
 	location = request.args.get('location')
 	splittedLocation = location.split(';')
-    latitude = splittedLocation[0]
-    longitude = splittedLocation[1]
+	latitude = splittedLocation[0]
+	longitude = splittedLocation[1]
 
 	request.post("http://api.justyo.co/yo/", data={'api_token': API_TOKEN, 'username': searchword })
+
+@app.route('/safe', methods = ['POST', 'GET'])
+def safe():
+	return render_template('safe.html')
+
+@app.route('/crime', methods = ['POST', 'GET'])
+def show():
+	lat = 42.360091
+	lon = -71.09415999999999
+	return render_template('index.html', lat=lat, lon=lon)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port = 5000)
