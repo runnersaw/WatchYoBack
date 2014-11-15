bb4ceabbe9d7157fe6d6e1d42902251ee4c44c69
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2014-11-15 13:42:09 -0500

    initial

--- web.py
+++ web.py
@@ -9,13 +9,10 @@ from flask import render_template, redirect, request, jsonify, url_for
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['POST'])
+@app.route('/', methods = ['GET', 'POST'])
 def begin():
-	return "Hello, world"
-
-@app.route('/', methods = ['GET'])
-def begin():
-	return render_template('index.html')
+	searchword = request.args.get('username', '')
+	request.post("http://api.justyo.co/yo/", data={'api_token': 'd1c5ad87-a91d-498f-89f1-f17c6b432b2b', 'username': 'SVAUGHAN'})
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
